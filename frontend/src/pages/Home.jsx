ddd916f40d1bb0934f2640449f8cb943f933cbaf
Abdul Azeem khan <abdulazeemkhan8879@gmail.com>
2026-01-20 19:52:11 +0530

    Rename CSS import to match file casing
    just capital letter change

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { Link, useSearchParams } from "react-router-dom";
 import api from "../services/api";
-import "./home.css";
+import "./Home.css";
 
 const Home = () => {
   const [products, setProducts] = useState([]);
