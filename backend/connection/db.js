4120ba79800b3fa6df869687d78a7aca7f113aa4
abdulazeem8879 <abdulazeemkhan8879@gmail.com>
2026-01-21 13:10:37 +0530

    update mongo connection

--- backend/connection/db.js
+++ backend/connection/db.js
@@ -1,15 +1,16 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-try {
-    await mongoose.connect(process.env.MONGO_URL)
-    console.log("MongoDB connected successfully");
-} catch (error) {
-    console.log(`MongoDB connection failed: ${error.message}`)
-}
-}
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URL, {
+      serverSelectionTimeoutMS: 5000, // ⏱ fast fail
+    });
 
-
-
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error("MongoDB connection error:", error.message);
+    process.exit(1); // 🔥 VERY IMPORTANT
+  }
+};
 
 export default connectDB;
